6580c96cf35df1a1c62b1f90bb30fb59e217d4e2
USER <EMAIL>
2021-11-11 12:13:18 +0700

    edit

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="con-txt">
+        <h2 >Test Page</h2>
+        <h2>text2</h2>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .con-txt {
+        text-align: center;
+    }
+</style>
